2f5367b1d5d94460df1a79558443c747722e46fb
David Kuperman <kuperman@192-168-1-108.tpgi.com.au>
2023-01-24 13:22:46 +1100

    updated with small changes to submit function

--- script.js
+++ script.js
@@ -1,47 +1,34 @@
 //VARIABLES LIST
 
 const words = ["aback", "abase", "abate", "abbey", "abbot", "abhor", "abide", "abled", "abode", "abort", "about", "above", "abuse", "abyss", "acorn", "acrid", "actor", "acute", "adage", "adapt"]
-let guessWord = words[Math.floor(Math.random() * words.length)]/*generates the random word that user must guess*/
-guessWord = guessWord.toUpperCase()
+let guessWord = words[Math.floor(Math.random() * words.length)].toUpperCase()/*generates the random word that user must guess*/
 console.log(guessWord)
+let usersWord = []
 
 let errorPrompt = document.querySelector('#gameMessage')
-console.log(errorPrompt)
-
 let getLetters = document.querySelectorAll('.key') // selects html element for keyboard keys
-
 let tiles = document.querySelectorAll('.tile') // select html element for grid tile
 let tileRow = document.querySelectorAll('.row')// select html element for tile row
-console.log(tileRow)
 
 let selectedTile = 0
+let selectedRow = 0
 
 /* write a function that checks the word in a row
  1.When each letter is passed into a tile, add those letters into an array. *done*
  2.join the array of letters to form a string (usersWords) *done*
- 3.check/match the usersword string with the actual word
+ 3.check/match the usersword string with the actual word *done*
+ 4. once the usersWord has been compared to the guessWord, if userWord is wrong progress to next row of tiles
+ 5.
 */
-
-let usersWord = []
-
-const checkInput = function () {
-    let input = usersWord.join('')
-    input = input.toUpperCase()
-    console.log(input)
-
-    /*if (selectedTile){
-     }else */if (selectedTile > 4) {
-        console.log('the user guessed ' + input + '. The actual word is ' + guessWord)
+     if (input.length !== 5) {
+        errorPrompt.textContent = 'You must guess a word with 5 letters'
+        console.log("not enough characters")
     }
     if (input === guessWord) {
         errorPrompt.textContent = 'Well done!'
-    } else if (selectedTile < 4) {
-        errorPrompt.textContent = 'You must guess a word with 5 letters'
-        console.log("not enough characters")
-    } 
-
-    
-}
+    } else {
+        //input code to move to the next tile row
+    }
 
 
 //input clicked keyboard innertext into tile grid
@@ -65,12 +52,12 @@ for (let i = 0; i < getLetters.length; i++) { //loops through all the keys and a
 
         }
         else {
+            let input = usersWord.join('')
+            input = input.toUpperCase()
             console.log('clicked ' + letter)
-            if (selectedTile == 5) { return; }
+            if (input.length == 5) { return; }
             tiles[selectedTile].textContent = letter
-
             usersWord.push(letter)
-
             console.log(usersWord)
             selectedTile += 1
         }
